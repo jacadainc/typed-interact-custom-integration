26b9aab2e75fa3bd032e79bc77fb2a0a5dadc9cb
Doron Guttman <dguttman@jacada.com>
2019-02-18 22:29:17 -0500

    * global declaration

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,17 @@
 import { ServerResponse } from "http";
 
-declare const context: IContext;
+declare global {
+    const context: IContext;
 
-declare interface IContext {
-    consoleOut: string[];
-    res: ServerResponse;
-    params: object;
-    variables: object;
-    script: IContextifyScript;
-    trace(message: string): void;
-    complete(status: number, data: any): void;
+    interface IContext {
+        consoleOut: string[];
+        res: ServerResponse;
+        params: any;
+        variables: any;
+        script: IContextifyScript;
+        trace(message: string): void;
+        complete(status: number, data: any): void;
+    }
+    
+    interface IContextifyScript { }
 }
-
-declare interface IContextifyScript { }
